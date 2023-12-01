7d4f6ce9e2faca84d7d58d6dada33cdad2c4d11b
Pedro Gamboa <pgamboa@elcentrochc.org>
2023-11-30 21:14:58 -0600

    logout button working

---
+++ public/js/logout.js
@@ -0,0 +1,16 @@
+const logout = async () => {
+    const response = await fetch('/api/users/logout', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to log out.');
+    }
+  };
+  
+  document.querySelector('#logoutBtn').addEventListener('click', logout);
+  console.log(logout, "you've been logged out")

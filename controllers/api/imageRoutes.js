bb919c62d79057edef98c32a8d8e368779e73735
Gabriel Viernes <viernesgabriel@gmail.com>
2023-12-05 01:40:42 -0600

    added route to post files using multer

--- controllers/api/imageRoutes.js
+++ controllers/api/imageRoutes.js
@@ -1,13 +1,30 @@
 const router = require('express').Router();
+const { v4: uuidv4 } = require('uuid')
 const {FileRef, User} = require('../../models');
 
-router.post('/', async (req, res) => {
+const multer = require('multer')
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/images/')
+    },
+    filename: function (req, file, cb) {
+        let id = uuidv4()
+        let newFilename = id.slice(id.length-5, id.length)
+        cb(null, `${newFilename}.png`)
+    }
+})
+const upload = multer ({ storage: storage})
+router.post('/', upload.single('upload'), async (req, res) => {
     try {
-        
+        res.send('File uploaded!')
     } catch (error) {
         console.error(error)
         res.status(500).json(error)
     }
 })
+
+async function writeFile(file) {
+    fs.writeFile("../../public/images", file)
+}
 
 module.exports = router;
